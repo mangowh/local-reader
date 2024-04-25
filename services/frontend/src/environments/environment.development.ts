21e0ad9f4ff1849abe300b3afd8dc142dea54a89
s.tambellini <s.tambellini@icloud.com>
2024-04-25 17:31:30 +0200

    feat: change apiUrl

--- services/frontend/src/environments/environment.development.ts
+++ services/frontend/src/environments/environment.development.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  apiUrl: "http://localhost:3000/api/v1",
-  graphqlApiUrl: "http://localhost:4000",
+  apiUrl: "http://localhost:3000",
 };
--- services/frontend/src/graphql/graphql.provider.ts
+++ services/frontend/src/graphql/graphql.provider.ts
@@ -4,7 +4,8 @@ import { ApplicationConfig, inject } from "@angular/core";
 import { ApolloClientOptions, InMemoryCache } from "@apollo/client/core";
 import { environment } from "../environments/environment";
 
-const uri = environment.graphqlApiUrl;
+const uri = environment.apiUrl;
+
 export function apolloOptionsFactory(): ApolloClientOptions<any> {
   const httpLink = inject(HttpLink);
   return {
